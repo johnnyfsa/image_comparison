99a33573526a291fd15e595db57a65a78ba99cd3
johnnyfsa <joaofeirense@gmail.com>
2022-05-12 00:18:39 -0300

    adaptando para placas do dataset

--- main.py
+++ main.py
@@ -3,30 +3,36 @@ import cv2
 import os
 import image_similarity_measures
 from sys import argv
-from image_similarity_measures.quality_metrics import rmse, ssim, sre
+from image_similarity_measures.quality_metrics import rmse, ssim, sre, fsim
 
 img = argv[1]
+
 
 test_img = cv2.imread('test/' + img)
 
 ssim_measures = {}
 rmse_measures = {}
 sre_measures = {}
+fsim_measures = {}
 
 scale_percent = 100 # percent of original img size
 width = int(test_img.shape[1] * scale_percent / 100)
 height = int(test_img.shape[0] * scale_percent / 100)
 dim = (width, height)
 
-data_dir = 'dataset'
-   
+data_dir = 'dataset/velocidade_60'
+
 for file in os.listdir(data_dir):
-	img_path = os.path.join(data_dir, file)
-	data_img = cv2.imread(img_path)
-	resized_img = cv2.resize(data_img, dim, interpolation = cv2.INTER_AREA)
-	ssim_measures[img_path]= ssim(test_img, resized_img)
-	rmse_measures[img_path]= rmse(test_img, resized_img)
-	sre_measures[img_path]= sre(test_img, resized_img)
+    img_path = os.path.join(data_dir, file)
+    data_img = cv2.imread(img_path)
+    if data_img is None:
+        print("null")
+    else:
+        resized_img = cv2.resize(data_img, dim, interpolation= cv2.INTER_AREA)
+    ssim_measures[img_path] = ssim(test_img, resized_img)
+    rmse_measures[img_path] = ssim(test_img, resized_img)
+    sre_measures[img_path] = ssim(test_img, resized_img)
+    fsim_measures[img_path] = ssim(test_img, resized_img)
 
 def calc_closest_val(dict, checkMax):
     result = {}
@@ -45,7 +51,9 @@ def calc_closest_val(dict, checkMax):
 ssim = calc_closest_val(ssim_measures, True)
 rmse = calc_closest_val(rmse_measures, False)
 sre = calc_closest_val(sre_measures, True)
+fsim = calc_closest_val(fsim_measures, True)
 
 print("The most similar according to SSIM: " , ssim)
 print("The most similar according to RMSE: " , rmse)
 print("The most similar according to SRE: " , sre)
+print("The most similar according to FSIM: " , fsim)
